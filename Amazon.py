eda332eb60579fac46bff9cde8f9af6327ddbed6
Igor Postnikov <igor.postnikov8@gmail.com>
2020-05-02 16:34:21 -0700

    added model and sets

--- Amazon.py
+++ Amazon.py
@@ -26,3 +26,80 @@ plt.plot(df['Close'])
 plt.xlabel('Date', fontsize=18)
 plt.ylabel('Close Price USD ($)', fontsize=18)
 plt.show()
+
+# Create a new dataframe with only the 'Close' column
+data = df.filter(['Close'])
+
+# Convert the dataframe to a numpy array
+dataset = data.values
+
+# Get the number of the rows to train the model on
+training_data_len = math.ceil(len(dataset) * .8)
+
+print(training_data_len)
+
+# Scale the data
+scaler = MinMaxScaler(feature_range=(0,1))
+scaled_data = scaler.fit_transform(dataset)
+
+print(scaled_data)
+
+# Create the training data set
+# Create the scaled training data set
+training_data = scaled_data[0:training_data_len , :]
+
+#Split tha data x_train and y_train data sets
+x_train = []
+y_train = []
+
+for i in range(60, len(training_data)):
+    x_train.append(training_data[i-60:i, 0])
+    y_train.append(training_data[i, 0])
+    if i <= 60:
+        print(x_train)
+        print(y_train)
+        print()
+
+# Convert the x_train and y_train to numpy arrays
+x_train, y_train = np.array(x_train), np.array(y_train)
+
+# Reshape the data
+x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
+print(x_train.shape)
+
+# Build the LSTM model
+model = Sequential()
+model.add(LSTM(50, return_sequences=True, input_shape=(x_train.shape[1], 1)))
+model.add(LSTM(50, return_sequences=False))
+model.add(Dense(25))
+model.add(Dense(1))
+
+# Compile the model
+model.compile(optimizer='adam', loss='mean_squared_error')
+
+# Train the model
+model.fit(x_train, y_train, batch_size=1, epochs=1)
+
+# Create the testing data set
+# Create a new array containing scaled values from index 1543 to 2003
+test_data = scaled_data[training_data_len - 60: , :]
+
+# Create the data sets x_test and y_sets
+x_test = []
+y_test = dataset[training_data_len: , :]
+for i in range(60, len(test_data)):
+    x_test.append(test_data[i-60:i, 0])
+
+# Convert the data to a numpy array
+x_test = np.array(x_test)
+
+# Reshape the data
+x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1 ))
+
+# Get the models predicted price values
+predictions = model.predict(x_test)
+predictions = scaler.inverse_transform(predictions)
+
+# Get the root mean squared error (RMSE)
+rmse = np.sqrt( np.mean( predictions - y_test )**2 )
+print(rmse)
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-## This program uses an artificial recurrent neural network called Long Short Term Memory (LSTM) to predict the closing stock price of a corporation (Amazon.com, Inc.) using the last 60 days stock price
+### This program uses an artificial recurrent neural network called Long Short Term Memory (LSTM) to predict the closing stock price of a corporation (Amazon.com, Inc.) using the last 60 days stock price
